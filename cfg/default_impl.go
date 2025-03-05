jgilman1337/gotils

commit e97d4439f97053c62615fc018886f3da59bc1bc0

Begin work on marshaling stuff; needs further work

cfg/default_impl.go
@@ -1,10 +1,10 @@
 package cfg
 
 import (
-	"encoding/json"
+	//"encoding/json"
 	"fmt"
 	"github.com/creasty/defaults"
-	"os"
+	//"os"
 	"reflect"
 )
 
@@ -13,7 +13,7 @@ type DefaultsProvider func() (IConfig, error)
 
 // Impelments a basic configuration object that contains a data struct, which holds thr actual configuration data.
 type Config[T any] struct {
-	Data T // The inner configuration data object.
+	data T // The inner configuration data object.
 
 	DFunc DefaultsProvider // The function that will set default values.
 }
@@ -24,8 +24,13 @@ var _ IConfig = (*Config[any])(nil)
 // Creates a new Config object using a data struct.
 func NewConfig[T any](data T) *Config[T] {
 	return &Config[T]{
-		Data: data,
+		data: data,
 	}
+}
+
+// Implements the Data() function from IConfig.
+func (c *Config[T]) Data() any {
+	return c.data //TODO: maybe use generics here to avoid unnecessary casts
 }
 
 // Implements the Defaults() function from IConfig. Uses creasty/defaults or a custom provider to provide the default object.
@@ -41,6 +46,7 @@ func (c *Config[T]) Defaults() (IConfig, error) {
 		}
 
 		//Use reflection to set the value
+		//TODO: make this a utility
 		v := reflect.ValueOf(c)
 		if v.Kind() != reflect.Ptr || v.IsNil() {
 			return nil, fmt.Errorf("c must be a non-nil pointer")
@@ -56,15 +62,18 @@ func (c *Config[T]) Defaults() (IConfig, error) {
 		v.Elem().Set(dv)
 		return c, nil
 	} else {
-		return c, defaults.Set(&c.Data)
+		return c, defaults.Set(&c.data)
 	}
 }
 
 // Implements the SaveAs() function from IConfig.
 func (c Config[T]) SaveAs(path string) error {
+	/*
 	data, err := json.Marshal(c.Data)
 	if err != nil {
 		return err
 	}
 	return os.WriteFile(path, data, 0644)
+	*/
+	return nil
 }

cfg/iface.go
@@ -5,6 +5,9 @@ Represents a generic configuration object that has many QoL features like defaul
 marshaling/unmarshaling to/from byte streams, and more.
 */
 type IConfig interface {
+	// Provides a writable pointer to the config data contained within an IConfig struct. 
+	Data() any
+	
 	// Sets the default values for this config object.
 	Defaults() (IConfig, error)
 

cfg/marshaler/base.go
@@ -3,7 +3,22 @@ package marshaler
 import "github.com/jgilman1337/gotils/cfg"
 
 // Defines the structure of a function that marshals a config object to a byte stream.
-type BMarshalerFunc func(c *cfg.IConfig) ([]byte, error)
+type MarshalerFunc func(c cfg.IConfig) ([]byte, error)
 
 // Defines the structure of a function that unmarshals a config object from a byte stream.
-type BUMarshalerFunc func(b []byte, c *cfg.IConfig) error
+type UMarshalerFunc func(b []byte, c cfg.IConfig) error
+
+// Represents a basic marshal and unmarshal object that can be used to read/write to/from config files.
+type Marshaler interface {
+	// The default path	to use when reading/writing.
+	DefaultPath() string
+	
+	// Converts a config object to a byte stream for writing to a file.
+	MFunc(c cfg.IConfig) ([]byte, error)
+	
+	// Indicates the priority of the marshaler. Higher numbers generally mean higher priority and run last when marshaling and unmarshaling.
+	Priority() int
+	
+	// Converts a byte stream to a config object for usage.
+	UFunc(b []byte, c cfg.IConfig) error
+}

cfg/marshaler/json.go
@@ -1,1 +1,32 @@
 package marshaler
+
+import (
+	"encoding/json"
+	"github.com/jgilman1337/gotils/cfg"
+)
+
+// Represents a JSON marshaler that implements Marshaler.
+type Json struct {}
+
+// Enforces compliance with the Marshaler interface.
+var _ Marshaler = (*Json)(nil)
+
+// Implements the DefaultPath() function from Marshaler.
+func (j Json) DefaultPath() string {
+	return "config.json"
+}
+
+// Implements the MFunc() function from Marshaler.
+func (j Json) MFunc(c cfg.IConfig) ([]byte, error) {
+	return json.Marshal(c.Data()) //TODO: data pass here
+}
+
+// Implements the Priority() function from Marshaler.
+func (j Json) Priority() int {
+	return 0
+}
+
+// Implements the UFunc() function from Marshaler.
+func (j Json) UFunc(b []byte, c cfg.IConfig) error {
+	return json.Unmarshal(b, &c.Data())
+}

cfg/marshaler/marshaler_test.go
@@ -1,0 +1,65 @@
+package marshaler
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/jgilman1337/gotils/cfg"
+	"testing"
+)
+
+// --- BEGIN test cfg obj
+type cfgtest struct {
+	Foo    string         `default:"hello world"`
+	Bar    int            `default:"42"`
+	FooBar map[string]int `default:"{\"foo\": 1, \"bar\": 2, \"baz\": 3}"`
+	Baz    []string       `default:"[\"foo\", \"bar\", \"baz\"]"`
+}
+//--- END test cfg obj
+
+var (
+	//The expected default value of a test config object
+	dat = cfg.NewConfig(cfgtest{
+		Foo:    "hello world",
+		Bar:    42,
+		FooBar: map[string]int{"foo": 1, "bar": 2, "baz": 3},
+		Baz:    []string{"foo", "bar", "baz"},
+	})
+	
+	//The expected output of a Json marshal
+	jout = []byte(`{"Foo":"hello world","Bar":42,"FooBar":{"bar":2,"baz":3,"foo":1},"Baz":["foo","bar","baz"]}`)
+	
+	//A Json marshaler instance for testing
+	mjson = Json{}
+)
+
+// Tests the marshaling function of the Json marshaler struct.
+func TestMarshalJson(t *testing.T){
+	//Run the test
+	actual, err := mjson.MFunc(dat)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("%s\n", actual)
+	
+	//Check for accuracy
+	if bytes.Compare(actual, jout) != 0 {
+		t.Fatalf("incorrect JSON marshal output; got `%s`, expected `%s`\n", actual, jout)
+	}
+}
+
+// Tests the unmarshaling function of the Json marshaler struct.
+func TestUMarshalJson(t *testing.T){
+	//Run the test
+	var actual cfg.Config[cfgtest]
+	if err := mjson.UFunc(jout, actual); err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("%+v\n", actual)
+	
+	/*
+	//Check for accuracy
+	if bytes.Compare(actual, jout) != 0 {
+		t.Fatalf("incorrect JSON marshal output; got `%s`, expected `%s`\n", actual, jout)
+	}
+	*/
+}

cfg/cfg_test.go
@@ -12,12 +12,6 @@ type cfgtest struct {
 	FooBar map[string]int `default:"{\"foo\": 1, \"bar\": 2, \"baz\": 3}"`
 	Baz    []string       `default:"[\"foo\", \"bar\", \"baz\"]"`
 }
-
-/*
-func (c *cfgtest) Defaults() (IConfig, error) {
-	return DefaultsHelper(c, nil)
-}
-*/
 //--- END test cfg obj
 
 var (

cfg/model.go
@@ -1,0 +1,39 @@
+package cfg
+
+/*
+Represents a generic configuration object that has many QoL features like default values,
+marshaling/unmarshaling to/from byte streams, and more.
+*/
+type IConfig interface {
+	/*
+		Sets the default values for this configuration object. Example function body
+		(assuming implementor is called Foo):
+
+			func (c *Foo) Defaults() (cfg.IConfig, error) {
+				return cfg.DefaultsHelper(c, nil)
+			}
+	*/
+	Defaults() (IConfig, error)
+
+	//SaveDefault() error
+	//Save() error
+	SaveAs(path string) error
+
+	//Load() (IConfig, error)
+	//LoadAs(path string) (IConfig, error)
+
+	//Init() (IConfig, error)
+	//InitAs(path string) (IConfig, error)
+
+	//defaultPathName() string //Specifies the default location to drop the default configuration.
+}
+
+//func SaveDefault() {}		// Saves the default config object to the default path.
+//func Save() {}			// Saves a config object to the default path.
+//func SaveAs() {}			// Saves a config object to a given path.
+//func Load() {}			// Saves a config object to the default path.
+//func LoadAs() {}			// Loads a config object from a given path.
+//func Init() {}			// Loads a config object from the default path, saving the default version if it doesn't exist.
+//func InitAs() {}			// Loads a config object from a given path, saving the default version if it doesn't exist.
+
+//TODO: try to use a custom struct tag `kname` that indicates the name of the key

commit 5fbb275d65d2ce6b4d4b5dd13b588dbc8e4dc974

Make interfaces generic for type safety
This commit also fixes the issue introduced in commit e97d443 where the marshaler package fails to compile

cfg/default_impl.go
@@ -3,23 +3,25 @@ package cfg
 import (
 	//"encoding/json"
 	"fmt"
+
 	"github.com/creasty/defaults"
+
 	//"os"
 	"reflect"
 )
 
 // Defines a default configuration provider function.
-type DefaultsProvider func() (IConfig, error)
+type DefaultsProvider[T any] func() (IConfig[T], error)
 
 // Impelments a basic configuration object that contains a data struct, which holds thr actual configuration data.
 type Config[T any] struct {
 	data T // The inner configuration data object.
 
-	DFunc DefaultsProvider // The function that will set default values.
+	DFunc DefaultsProvider[T] // The function that will set default values.
 }
 
 // Enforces compliance with the IConfig interface.
-var _ IConfig = (*Config[any])(nil)
+var _ IConfig[any] = (*Config[any])(nil)
 
 // Creates a new Config object using a data struct.
 func NewConfig[T any](data T) *Config[T] {
@@ -29,12 +31,12 @@ func NewConfig[T any](data T) *Config[T] {
 }
 
 // Implements the Data() function from IConfig.
-func (c *Config[T]) Data() any {
-	return c.data //TODO: maybe use generics here to avoid unnecessary casts
+func (c *Config[T]) Data() *T {
+	return &c.data
 }
 
 // Implements the Defaults() function from IConfig. Uses creasty/defaults or a custom provider to provide the default object.
-func (c *Config[T]) Defaults() (IConfig, error) {
+func (c *Config[T]) Defaults() (IConfig[T], error) {
 	//return &Config[T]{Data: Zero[T]()}, nil
 
 	//Use the defaults provider if set

cfg/iface.go
@@ -4,12 +4,12 @@ package cfg
 Represents a generic configuration object that has many QoL features like default values,
 marshaling/unmarshaling to/from byte streams, and more.
 */
-type IConfig interface {
+type IConfig[T any] interface {
 	// Provides a writable pointer to the config data contained within an IConfig struct.
-	Data() any
+	Data() *T
 
 	// Sets the default values for this config object.
-	Defaults() (IConfig, error)
+	Defaults() (IConfig[T], error)
 
 	// Saves the default config object to the default path.
 	// SaveDefault() error

cfg/marshaler/base.go
@@ -3,22 +3,22 @@ package marshaler
 import "github.com/jgilman1337/gotils/cfg"
 
 // Defines the structure of a function that marshals a config object to a byte stream.
-type MarshalerFunc func(c cfg.IConfig) ([]byte, error)
+type MarshalerFunc[T any] func(c cfg.IConfig[T]) ([]byte, error)
 
 // Defines the structure of a function that unmarshals a config object from a byte stream.
-type UMarshalerFunc func(b []byte, c cfg.IConfig) error
+type UMarshalerFunc[T any] func(b []byte, c *cfg.IConfig[T]) error
 
 // Represents a basic marshal and unmarshal object that can be used to read/write to/from config files.
-type Marshaler interface {
+type Marshaler[T any] interface {
 	// The default path	to use when reading/writing.
 	DefaultPath() string
 
 	// Converts a config object to a byte stream for writing to a file.
-	MFunc(c cfg.IConfig) ([]byte, error)
+	MFunc(c cfg.IConfig[T]) ([]byte, error)
 
 	// Indicates the priority of the marshaler. Higher numbers generally mean higher priority and run last when marshaling and unmarshaling.
 	Priority() int
 
 	// Converts a byte stream to a config object for usage.
-	UFunc(b []byte, c cfg.IConfig) error
+	UFunc(b []byte, c cfg.IConfig[T]) error
 }

cfg/marshaler/json.go
@@ -2,31 +2,32 @@ package marshaler
 
 import (
 	"encoding/json"
+
 	"github.com/jgilman1337/gotils/cfg"
 )
 
 // Represents a JSON marshaler that implements Marshaler.
-type Json struct{}
+type Json[T any] struct{}
 
 // Enforces compliance with the Marshaler interface.
-var _ Marshaler = (*Json)(nil)
+var _ Marshaler[any] = (*Json[any])(nil)
 
 // Implements the DefaultPath() function from Marshaler.
-func (j Json) DefaultPath() string {
+func (j Json[T]) DefaultPath() string {
 	return "config.json"
 }
 
 // Implements the MFunc() function from Marshaler.
-func (j Json) MFunc(c cfg.IConfig) ([]byte, error) {
-	return json.Marshal(c.Data()) //TODO: data pass here
+func (j Json[T]) MFunc(c cfg.IConfig[T]) ([]byte, error) {
+	return json.Marshal(c.Data())
 }
 
 // Implements the Priority() function from Marshaler.
-func (j Json) Priority() int {
+func (j Json[T]) Priority() int {
 	return 0
 }
 
 // Implements the UFunc() function from Marshaler.
-func (j Json) UFunc(b []byte, c cfg.IConfig) error {
-	return json.Unmarshal(b, &c.Data())
+func (j Json[T]) UFunc(b []byte, c cfg.IConfig[T]) error {
+	return json.Unmarshal(b, c.Data())
 }

cfg/marshaler/marshaler_test.go
@@ -3,8 +3,9 @@ package marshaler
 import (
 	"bytes"
 	"fmt"
-	"github.com/jgilman1337/gotils/cfg"
 	"testing"
+
+	"github.com/jgilman1337/gotils/cfg"
 )
 
 // --- BEGIN test cfg obj
@@ -30,7 +31,7 @@ var (
 	jout = []byte(`{"Foo":"hello world","Bar":42,"FooBar":{"bar":2,"baz":3,"foo":1},"Baz":["foo","bar","baz"]}`)
 
 	//A Json marshaler instance for testing
-	mjson = Json{}
+	mjson = Json[cfgtest]{}
 )
 
 // Tests the marshaling function of the Json marshaler struct.
@@ -52,7 +53,7 @@ func TestMarshalJson(t *testing.T) {
 func TestUMarshalJson(t *testing.T) {
 	//Run the test
 	var actual cfg.Config[cfgtest]
-	if err := mjson.UFunc(jout, actual); err != nil {
+	if err := mjson.UFunc(jout, &actual); err != nil { //TODO: add equals function to the interface, which deep compares against the `Data()` field of both interfaces
 		t.Fatal(err)
 	}
 	fmt.Printf("%+v\n", actual)